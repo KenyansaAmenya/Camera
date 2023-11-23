70c5cf9d8b5a148b4a4c2b51a8fe5075f164d3bc
KenyansaAmenya <100693771+KenyansaAmenya@users.noreply.github.com>
2023-11-23 18:55:54 +0300

    Camera feature in a website

---
+++ script.js
@@ -0,0 +1,9 @@
+var cameraFeed = document.getElementById('cameraFeed');
+
+navigator.mediaDevices.getUserMedia({video: true})
+.then(stream => {
+    cameraFeed.srcObject = stream;
+})
+.catch(error => {
+    console.error('Error accessing camera:', error);
+});
